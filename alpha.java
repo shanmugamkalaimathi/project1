64e547f3077c31c40627da1b7c58d701ae9ce5e1
shanmugamkalaimathi <kalai1113413@gmail.com>
2017-06-07 11:52:48 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,20 @@
+package p1;
+import java.util.Scanner;
+public class alpha {
+        public static void main(String[] args){
+	        char ch;
+	        Scanner scan = new Scanner(System.in);
+			
+	        System.out.print("Enter a Character : ");
+	        ch = scan.next().charAt(0);
+			
+	        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+	        {
+	            System.out.print(ch + " is an Alphabet");
+	        }
+	        else
+	        {
+	            System.out.print(ch + " is not an Alphabet");
+	        }
+	    }
+}
