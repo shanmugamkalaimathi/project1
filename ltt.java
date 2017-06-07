9dfffe54ad8ac56d3b82c71afd92e0b11cf5c017
shanmugamkalaimathi <kalai1113413@gmail.com>
2017-06-07 12:07:40 +0530

    Add files via upload

---
+++ ltt.java
@@ -0,0 +1,17 @@
+package p1;
+import java.util.Scanner;
+public class ltt {
+        public static void main(String[] args){
+			int n,i,b,a=0;
+			Scanner input = new Scanner(System.in);
+			System.out.println("the number is:");
+			n = input.nextInt();
+			while(n!=0){
+						
+							b=n%10;
+							a=a+b;
+							n=n/10;				
+			}
+			System.out.println("the sum of  digits are:"+a);
+		}
+}
