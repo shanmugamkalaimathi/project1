32cee1e6b5a0e8d8cbaa4a43b83fb6d52739ff0a
shanmugamkalaimathi <kalai1113413@gmail.com>
2017-06-30 14:35:47 +0530

    Add files via upload

---
+++ StringReverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class StringReverse {
+		public static void main(String[] args) {
+				Scanner in = new Scanner(System.in);
+				String str1=in.next();
+				StringBuffer s=new StringBuffer(str1).reverse();
+				
+					System.out.println("String Reverse:"+s);
+			}
+				
+				}
+			
+
+
+
+
