e253068f171e21a34e34aa2d5f6a1c30a17bc847
shanmugamkalaimathi <kalai1113413@gmail.com>
2017-06-30 11:54:14 +0530

    Add files via upload

---
+++ FindArrayIndex.java
@@ -0,0 +1,34 @@
+package lc3;
+import java.util.Scanner;
+public class FindArrayIndex {
+    public static void main(String[] args){
+			int size,flag =0,i=0;
+			Scanner in = new Scanner(System.in);
+			size = in.nextInt();
+			int[] array=new int[size];
+			for(i=0;i<size;i++){
+				array[i] = in.nextInt();	
+		        }
+		        int given = in.nextInt();
+		       for( i=0;i<size;i++){
+		    	   int a = array[i];
+		       if(a==given){
+		    	   flag =1;
+		    	   break;
+		       }
+		       else{
+		    	   flag =0 ;
+		       }
+		       }
+		       if(flag==1){
+		       System.out.println("index values of array:"+( i ));
+		}
+		       else{
+		System.out.println("-1");
+		      
+		       }
+		}
+			
+	}
+
+
