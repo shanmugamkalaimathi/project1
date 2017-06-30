7604576a07d58df17f6ba8efcdf7d7cfbe678937
shanmugamkalaimathi <kalai1113413@gmail.com>
2017-06-30 12:30:27 +0530

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+ public class Print {
+		public static void main(String[] args){
+			int n;
+			int flag=1;
+			int [] b=new int[5];
+			int count=0;
+			int sum=0,average;
+			Scanner in = new Scanner(System.in) ;
+			n = in.nextInt();
+			int[] a=new int [n];
+			
+			for(int i=0;i<n;i++){
+				a[i]=in.nextInt();
+				
+			}
+	        for(int i = 0; i < n; i++)
+	        {
+	            for(int j = 0; j < n; j++)
+	            {
+	                if(i != j)
+	                {
+	                    if(a[i] != a[j])
+	                    {
+	                        flag = 1;
+	                    }
+	                    else
+	                   {
+	                       flag = 0;
+	                        break;
+	                    }
+	                }
+	            }
+	            if(flag == 1)
+	            {
+	                count++;
+	                System.out.print(a[i]+" ");
+	            }
+	        }
+	        System.out.println("");
+	      
+	    }
+
+	}
